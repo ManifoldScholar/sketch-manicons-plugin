a0f9a9798f30855702aa595300a64d4426c1d7f6
Zach Davis <zach@castironcoding.com>
2019-04-04 12:22:39 -0700

    [B] Address duplicat paths issue

--- src/stages/export-artboards.js
+++ src/stages/export-artboards.js
@@ -3,8 +3,9 @@ import sketch from 'sketch'
 export default function exportArtboards(artboards, target) {
 
   artboards.forEach((artboard) => {
-    sketch.export(artboards, {
+    sketch.export(artboard, {
       formats: "svg",
+      ["group-contents-only"]: true,
       output: `${target}`
     })
   })
